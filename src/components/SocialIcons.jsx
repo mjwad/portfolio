49dc9cc44419b335bad2f797f7ae2270957009f0
mjwad <jawaddmuhammad@gmail.com>
2024-06-12 22:10:00 +0500

    add upwork profile link

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { ReactComponent as Upwork }from "../images/upwork.svg";
 
 /**
  * Represents a component displaying social media icons.
@@ -21,6 +22,25 @@ const SocialIcons = () => {
 
   return (
     <div className="socialIcons" style={styles.socialIcons}>
+      <a className="icon h-10 w-10" style={styles.icon} href="https://www.upwork.com/freelancers/~01de9422cf0d596fe6?mp_source=share">
+        {/* Upwork Icon */}
+        <motion.svg
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1.8, duration: 0.5, type: "spring" }}
+          style={{
+            height: "23px",
+            width: "23px",
+            backgroundColor: "#f0f0f0", // Replace with your desired background color
+            borderRadius: "2px",
+            padding: "2px 2px 0px 2px"
+          }}
+          aria-hidden="true"
+          title="Muhammad Jawad's Upwork Profile"
+        >
+          <Upwork />
+        </motion.svg>
+      </a>
       <a className="icon" style={styles.icon} href="https://github.com/mjwad">
         {/* GitHub Icon */}
         <motion.i
@@ -43,7 +63,7 @@ const SocialIcons = () => {
           title="Muhammad Jawad' LinkedIn Profile"
         ></motion.i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.instagram.com/_m_jawad_malik/">
+      <a className="icon" style={styles.icon} href="https://www.instagram.com/mjwad/">
         {/* Instagram Icon */}
         <motion.i
           initial={{ y: 100, opacity: 0 }}
