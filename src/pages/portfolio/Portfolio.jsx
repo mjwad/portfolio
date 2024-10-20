b9e5f3fd3be1fbbec63871f70179f983bd72d763
mjwad <jawaddmuhammad@gmail.com>
2024-10-20 23:30:31 +0500

    feat: adds projects count and scrolling indicator

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,36 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import PageHeader from "../../components/PageHeader";
 import ProjectList from "../../components/ProjectList";
 import Footer from "../../components/Footer";
-
-/**
- * Represents the Portfolio page component.
- * Displays a list of projects and allows users to view the creator's work.
- *
- * @component
- */
+import projects from "../../_data/projects.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Portfolio = () => {
-  // Get the current location using React Router's useLocation hook
   const location = useLocation();
+  const [isVisible, setIsVisible] = useState(true);
 
-  // Scroll to the top of the page when the location changes
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    // Show the icon until the user reaches the bottom of the page
+    if (scrollPosition < documentHeight - 50) { // Adjust the threshold as needed
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: window.innerHeight, // Adjust this value as needed for scrolling
+      behavior: 'smooth' // Smooth scrolling
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      {/* Main Portfolio Page */}
       <main className="portfolio container">
-        {/* Display the page header */}
-        <PageHeader title="Portfolio" description="View my work" />
+        <PageHeader title={`Portfolio (${projects.length}+)`} description="View my work" />
 
         <div className="row">
-          {/* Display the list of projects */}
           <ProjectList />
         </div>
+
+        {isVisible && (
+          <div className="scroll-indicator" onClick={handleClick}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+        )}
       </main>
       <Footer />
     </>
--- src/pages/portfolio/[project]/ProjectDetails.jsx
+++ src/pages/portfolio/[project]/ProjectDetails.jsx
@@ -8,29 +8,15 @@ import Image from "../../../components/Image";
 import projects from "../../../_data/projects.json";
 import Footer from "../../../components/Footer";
 
-/**
- * Represents the ProjectDetails page component.
- * Displays details of a specific project.
- *
- * @component
- */
-
 const ProjectDetails = () => {
-  // Get the current location using React Router's useLocation hook
   const location = useLocation();
-
-  // Scroll to the top of the page when the location changes
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  // Get the project title from the route parameters
   const { projectTitle } = useParams();
-
-  // Find the project in the data using the title
   const project = projects.find((project) => project.title.toLowerCase() === projectTitle);
 
-  // If the project is not found, display the PageNotFound component
   if (!project) {
     return <PageNotFound />;
   }
@@ -38,17 +24,14 @@ const ProjectDetails = () => {
   return (
     <>
       <main className="container portfolio">
-        {/* Display the page header with project title and description */}
         <PageHeader title={project.title} description={project.description} />
         <div className="projectDetails">
           <div className="row">
             <div className="col-12 col-xl-4 projectImage">
-              {/* Display the project image */}
               <Image src={project.image2} alt={project.name} opacity="0.5" />
             </div>
             <div className="col-12 col-xl-8 projectBodyContainer">
               <div className="tech">
-                {/* Display project technologies with animation */}
                 {project.technologies.map((technology, i) => (
                   <motion.span
                     key={i}
@@ -63,7 +46,6 @@ const ProjectDetails = () => {
               </div>
 
               <div className="projectBody">
-                {/* Display project body paragraphs with animation */}
                 {project.body.split("\n").map((paragraph, i) => (
                   <motion.p
                     className="paragraph"
@@ -77,7 +59,6 @@ const ProjectDetails = () => {
                 ))}
               </div>
 
-              {/* Display buttons with animation */}
               <motion.div
                 style={{ display: "flex", gap: "10px" }}
                 initial={{ opacity: 0, y: 20 }}
